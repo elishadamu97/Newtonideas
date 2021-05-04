bd6a772962eed733f795542504ebdff83813c261
elishadamu97 <40290364+elishadamu97@users.noreply.github.com>
2021-05-05 00:46:03 +0100

    Add files via upload

---
+++ index/linkup.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = alert("Your information has been sent, thanks for Contacting us");
+    document.getElementById("demo").style.visibility='hidden';
+  }
+
+  
